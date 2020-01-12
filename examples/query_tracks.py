8ab18ad2ea00bea8d7795bbdab2b5f6893f3f57d
dexter2206 <dexter2206@gmail.com>
2020-01-12 11:39:23 +0100

    Add example for querying tracks

---
+++ examples/query_tracks.py
@@ -0,0 +1,14 @@
+from models import Track
+from db import session_scope, Session, get_engine
+
+
+if __name__ == "__main__":
+    engine = get_engine()
+    Session.configure(bind=engine)
+
+    with session_scope() as session:
+        track = session.query(Track)[2]
+        print("Name:", track.name)
+        print("Album:", track.album.title)
+        print("Genre:", track.genre.name)
+        print("Media type:", track.media_type.name)
