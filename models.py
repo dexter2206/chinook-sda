094f699ff65cbc22b7fa0fb244ea01704d78eceb
dexter2206 <dexter2206@gmail.com>
2020-01-12 11:39:04 +0100

    Add Track model

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -23,6 +23,7 @@ class Album(Base):
     artist_id = Column(Integer, ForeignKey("Artist.ArtistId"), name="ArtistId")
 
     artist = relationship(Artist, back_populates="albums")
+    tracks = relationship("Track", back_populates="album")
 
     def __repr__(self):
         return f"<Album(id={self.id}, title={self.title})>"
@@ -38,3 +39,23 @@ class Genre(Base):
     __tablename__ = "Genre"
     id = Column(Integer, primary_key=True, name="GenreId")
     name = Column(String(120), name="Name")
+
+
+class Track(Base):
+    __tablename__ = "Track"
+    id = Column(Integer, primary_key=True, name="TrackId")
+    name = Column(String(200), name="Name")
+    album_id = Column(Integer, ForeignKey("Album.AlbumId"), name="AlbumId")
+    media_type_id = Column(Integer, ForeignKey("MediaType.MediaTypeId"), name="MediaTypeId")
+    genre_id = Column(Integer, ForeignKey("Genre.GenreId"), name="GenreId")
+    composer = Column(String(220), name="Composer")
+    milliseconds = Column(Integer, name="Milliseconds")
+    bytes = Column(Integer,name="Bytes")
+    unit_price = Column(Float, name="UnitPrice")
+
+    album = relationship(Album, back_populates="tracks")
+    genre = relationship(Genre)
+    media_type = relationship(MediaType)
+
+    def __repr__(self):
+        return f"<Track(name='{self.name}')>"
