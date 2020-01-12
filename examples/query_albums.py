270eebe62aa4217d7ad63cd1cfc7a3140b4081bd
dexter2206 <dexter2206@gmail.com>
2020-01-12 10:10:04 +0100

    Add example for querying albums

---
+++ examples/query_albums.py
@@ -0,0 +1,17 @@
+from models import Album, Artist
+from db import get_engine, session_scope, Session
+
+
+if __name__ == "__main__":
+    engine = get_engine(echo=True)
+    Session.configure(bind=engine)
+
+    with session_scope() as session:
+        print("Pierwsze dwa albumy:")
+        for album in session.query(Album).limit(2):
+            print(f"{album.artist.name}: {album.title}")
+
+        artist = session.query(Artist).first()
+        print(f"Albumy artysty {artist.name}:")
+        for album in artist.albums:
+            print(f"- {album.title}")
